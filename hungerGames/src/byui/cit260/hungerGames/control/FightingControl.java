744c837d957674fd36b606ab3e7218f07dd75bb1
Carlee Ouwerkerk <Carlee Ouwerkerk@10.18.149.62>
2015-05-26 10:55:41 -0600

    05/14/15 Added Location class

---
+++ hungerGames/src/byui/cit260/hungerGames/control/FightingControl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hungerGames.control;
+
+/**
+ *
+ * @author Carlee Ouwerkerk
+ */
+public class FightingControl {
+    
+    static public boolean calculateSneak(double player, double scene, double tribute){
+        
+        //Sneak paramaters
+        if (player < 0 || player > 51) {
+            return false;
+        }
+        
+        if (scene < -3 || scene > 3) {
+            return false;
+        }
+        
+        if (tribute < 5 || tribute > 45) {
+            return false;
+        }
+        
+        //Calculate sneak
+        if ((player + scene) * playerRandomFactor) < (tribute * tributeRandomFactor) {
+            return false;
+        }
+        
+        if ((player + scene) * playerRandomFactor) > (tribute * tributeRandomFactor) {
+            return true;
+        }
+
+    }
+    
+}
