6db43194dd48c0bee4c13feac02513d171e5bf4f
Carlee Ouwerkerk <Carlee Ouwerkerk@CarleeOuwerkerk>
2015-07-18 17:24:42 -0600

    Sorry, I couldn' help

--- hungerGames/src/byui/cit260/hungerGames/control/TrapControl.java
+++ hungerGames/src/byui/cit260/hungerGames/control/TrapControl.java
@@ -32,21 +32,36 @@ public class TrapControl {
         if (angle != 10) {
             return false;
         }
+        //I think we should change it so meat is in every location, that way you can set a trap anywhere
+        //or, this may make thing easier, we could make it so there is ininity meat in every location
+        //that way, we wouldn't have to check if there was meat.  This would make it easy to cheat, but
+        //who's going to be really playing this?
+        
+        //we need something  here to check if there is an assigned meat item in this location.
+        //if there isn't, we should println(You have already set a trap here) and return to the game menu
+        //if there is meat available, we should proceed with this function
         double heightDistance = Math.pow(height, 2) + Math.pow(distance, 2);
         if (heightDistance == Math.pow(angle, 2)) {
          
+            //We need to subtract one from the assigned meat item
            AssignedItem assignedItem = HungerGames.getPlayer().getLocation().getAssignedItem();
             
+           //I'm not sure what this is doing
            // get assigned amount
            assignedItem.setAmount(assignedItem.getAmount() - 1);
     
         // get inventory
+           
+           //this will add meat to inventory from the trap
+           meat.amount = meat.getAmount() + 1;
           
            
+           //again, I'm not sure what this is doing
 //           if inventory was found
             // new assignedlist for meat
                 //set amount to 1
            
+           //or what this is for
 //           else 
                 // get assigneditem for meat
                     //set amount(currentamount +1)
@@ -58,6 +73,7 @@ public class TrapControl {
 
     }
     
+    //i understand that this goes through the assigneditems to find meat, but I don't understand why it does that
     public static AssignedItem searchList(Item[] items, Item item) {
         
         AssignedItem assignedItem = HungerGames.getPlayer().getLocation().getAssignedItem();
